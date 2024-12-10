f81d17c57924be9ca9826b3ee734ed5a7107341e
RosaMGini <rosa.m.gini@gmail.com>
2024-12-10 20:42:33 +0100

    results analysis

--- p_steps/06_T2_20_create_study_population.R
+++ p_steps/06_T2_20_create_study_population.R
@@ -26,14 +26,16 @@ if (TEST){
 
 cohort <- readRDS(file.path(thisdirinput, "D3_source_population.rds"))
 events <- readRDS(file.path(thisdirinput, "D3_bleeding_events.rds"))
+persons <- readRDS(file.path(thisdirinput, "D3_PERSONS.rds"))
 obsperiods <- readRDS(file.path(thisdirinput, "D3_clean_spells.rds"))
 obsperiods <- obsperiods[is_the_study_spell == 1,]
 
 # process: cohort is excluding already 30 days after all events 
 
 processing <- merge(cohort,events, by = "person_id", all = F)
-processing <- processing[date >= entry_cohort & date <= exit_cohort,]
-processing <- processing[,.(person_id,birthdate,sex,date,event)]
+processing <- processing[date >= study_start_date & date >= entry_cohort & date <= exit_cohort,]
+processing <- processing[,.(person_id,birth_date,gender,date,event)]
+processing <- merge(processing,persons[,.(person_id,death_date)], by = "person_id", all = F)
 processing <- merge(processing,obsperiods, by = "person_id", all = F)
 processing <- processing[date >= entry_spell_category & date <= exit_spell_category,]
 processing[, end_followup_d := pmin(exit_spell_category, date + 30)]
@@ -44,13 +46,13 @@ setnames(processing,"date","date_bleeding")
 ################################
 # clean
 
-tokeep <- c("person_id","birth_date","sex","death_date","date_bleeding","event","end_followup_d")
+tokeep <- c("person_id","birth_date","gender","death_date","date_bleeding","event","end_followup_d")
 
 processing <- processing[, ..tokeep]
 
 setorderv(
   processing,
-   c("person_id","date_bleeding")
+  c("person_id","date_bleeding")
 )
 
 
