d2388f1c286d41bfdd2248439bf638b6eda5cf89
sabrinagiometto <sabrina.giometto@gmail.com>
2024-12-16 14:30:42 +0100

    sensitivity analysis restricting to those with only one emorragic event
    thromboembolic outcome added

--- p_steps/08_T4_40_analysis.R
+++ p_steps/08_T4_40_analysis.R
@@ -8,6 +8,8 @@
 
 # authors: Rosa Gini, Ersilia Lucenteforte, Sabrina Giometto
 
+# v 0.4 16 Dec 2024 - Sensitivity analysis added
+
 # v 0.3 11 Dec 2024
 
 # exposure as factor
@@ -35,8 +37,12 @@ input <- readRDS(file.path(thisdirinput, "D4_analytical_dataset.rds"))
 
 # analysis
 input <- input %>% 
-           mutate(time = as.numeric(ceiling((date_bleeding+1 - as.Date("2018-01-01"))/30)),
+           mutate(time = as.numeric(ceiling((date_bleeding - as.Date("2018-01-01"))/30)),
                   month = month(date_bleeding))
+
+input <- input %>% 
+           mutate(time = case_when(time==0 ~ 1,
+                                   TRUE ~ as.numeric(time)))
 
 fix_time <- data.frame(
              time = as.numeric(1:84)) %>% 
@@ -57,20 +63,22 @@ den_narrow <- input %>%
                 group_by(time) %>% 
                 summarise(n_emor_narrow = n())
 
-den_broad <- input %>%
-               filter(type_bleeding=="possible") %>% 
-               group_by(time) %>% 
-               summarise(n_emor_broad = n())
+den_poss <- input %>%
+                  filter(type_bleeding=="possible") %>% 
+                  group_by(time) %>% 
+                  summarise(n_emor_poss = n())
 
 # creo un dataset dove l'unità è il tempo (mese)
-outcome_vars <- grep("^outcome", names(input), value = TRUE)
-             
+
+# outcome_vars <- grep("^outcome", names(input), value = TRUE)
+outcome_vars <- c("outcome_THROM", "outcome_DEATH")
+
 results <- map(outcome_vars, function(var) {
   
   input %>%
     group_by(time) %>% 
     summarise(event_narrow = sum(.data[[var]]*(type_bleeding == "narrow"), na.rm = T),
-              event_broad = sum(.data[[var]]*(type_bleeding == "possible"), na.rm = T)) %>% 
+              event_poss = sum(.data[[var]]*(type_bleeding == "possible"), na.rm = T)) %>% 
     mutate(period = case_when(time <= 26 ~ "1a",
                               (time > 26 & time <= 41) ~ "1b",
                               (time > 41 & time <= 44) ~ "1c",
@@ -96,9 +104,9 @@ for (i in names(results)) {
                          mutate(month = 1:12) %>% 
                          relocate(month, .after = year) %>%
                          # ungroup() %>%
-                         left_join(den_broad, by = "time") %>%
+                         left_join(den_poss, by = "time") %>%
                          left_join(den_narrow, by = "time") %>%
-                         mutate(prop_broad = event_broad/n_emor_broad,
+                         mutate(prop_poss = event_poss/n_emor_poss,
                                 prop_narrow = event_narrow/n_emor_narrow)
 
 }
@@ -111,7 +119,7 @@ combined_data <- bind_rows(lapply(names(results_updated), function(name) {
 png(file.path(thisdiroutput,"outcomes_possible.png"), units="in", height=10, width=15, res=300)
 
 # scatter plot separati per outcome (nel tempo)
-ggplot(combined_data, aes(x = time, y = prop_broad)) +
+ggplot(combined_data, aes(x = time, y = prop_poss)) +
   geom_line(aes(color = outcome), size = 1) +
   geom_point(size = 1) +
   labs(
@@ -152,14 +160,14 @@ fit_models <- function(data) {
   list(
     # periodo
     fit_periodo_narrow <- summary(glm(cbind(event_narrow, n_emor_narrow) ~ period, family = binomial, data = data)),
-    fit_periodo_broad <- summary(glm(cbind(event_broad, n_emor_broad) ~ period, family = binomial, data = data)),
+    fit_periodo_poss <- summary(glm(cbind(event_poss, n_emor_poss) ~ period, family = binomial, data = data)),
     # stagionalità (considero un df per stagione, 4 all'anno, dove df = numero di nodi interni + 1)
     # se usassi una natural spline ns, terrei conto allo stesso tempo di trend e stagionalità mentr con pbs riesco ad isolare la stagionalità
     fit_per_stag_narrow <- summary(glm(cbind(event_narrow, n_emor_narrow) ~ pbs(month, df = 4) + period, family = binomial, data = data)),
-    fit_per_stag_broad <- summary(glm(cbind(event_broad, n_emor_broad) ~ pbs(month, df = 4) + period, family = binomial, data = data)),
+    fit_per_stag_poss <- summary(glm(cbind(event_poss, n_emor_poss) ~ pbs(month, df = 4) + period, family = binomial, data = data)),
     # trend e stagionalità (con pbs)
     fit_per_stag_trend_narrow <- summary(glm(cbind(event_narrow, n_emor_narrow) ~ pbs(month, df = 4) + time + period, family = binomial, data = data)),
-    fit_per_stag_trend_broad <- summary(glm(cbind(event_broad, n_emor_broad) ~ pbs(month, df = 4) + time + period, family = binomial, data = data))
+    fit_per_stag_trend_poss <- summary(glm(cbind(event_poss, n_emor_poss) ~ pbs(month, df = 4) + time + period, family = binomial, data = data))
   )
   
 }
@@ -172,23 +180,185 @@ save(model_results, file = file.path(thisdiroutput,"model_results.rda"))
 
 model_results_indiv <- list(
   
-  fit_indiv_narrow_mixed <- glmer(outcome_DEATH ~ period + age + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input),
-  fit_indiv_narrow_fixed <- glm(outcome_DEATH ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input),
-  robust_se_cluster <- coeftest(fit_indiv_narrow_fixed, vcov = vcovCL(fit_indiv_narrow_fixed, cluster = input$person_id[which(input$type_bleeding=="narrow")])),
-  fit_indiv_narrow_mixed_agecat <- glmer(outcome_DEATH ~ period + ageband + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input),
-  fit_indiv_narrow_fixed_agecat <- glm(outcome_DEATH ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input),
-  robust_se_cluster <- coeftest(fit_indiv_narrow_fixed_agecat, vcov = vcovCL(fit_indiv_narrow_fixed_agecat, cluster = input$person_id[which(input$type_bleeding=="narrow")]))
-)
+  fit_indiv_narrow_mixed <- summary(glmer(outcome_DEATH ~ period + age + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input)),
+  fit_indiv_narrow_fixed <- summary(glm(outcome_DEATH ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input)),
+  robust_se_cluster <- coeftest(glm(outcome_DEATH ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input), vcov = vcovCL(glm(outcome_DEATH ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input), cluster = input$person_id[which(input$type_bleeding=="narrow")])),
+  fit_indiv_narrow_mixed_agecat <- summary(glmer(outcome_DEATH ~ period + ageband + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input)),
+  fit_indiv_narrow_fixed_agecat <- summary(glm(outcome_DEATH ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input)),
+  robust_se_cluster <- coeftest(glm(outcome_DEATH ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input), vcov = vcovCL(glm(outcome_DEATH ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input), cluster = input$person_id[which(input$type_bleeding=="narrow")])),
+  
+  fit_indiv_narrow_mixed <- summary(glmer(outcome_THROM ~ period + age + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input)),
+  fit_indiv_narrow_fixed <- summary(glm(outcome_THROM ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input)),
+  robust_se_cluster <- coeftest(glm(outcome_THROM ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input), vcov = vcovCL(glm(outcome_THROM ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input), cluster = input$person_id[which(input$type_bleeding=="narrow")])),
+  fit_indiv_narrow_mixed_agecat <- summary(glmer(outcome_THROM ~ period + ageband + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input)),
+  fit_indiv_narrow_fixed_agecat <- summary(glm(outcome_THROM ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input)),
+  robust_se_cluster <- coeftest(glm(outcome_THROM ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input), vcov = vcovCL(glm(outcome_THROM ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input), cluster = input$person_id[which(input$type_bleeding=="narrow")]))
+  
+  )
+  
 
-names(model_results_indiv) <- c("Effetti misti con età in continuo - Caso narrow",
-                                "Effetti fissi con età in continuo - Caso narrow",
-                                "Effetti fissi con età in continuo e SE robusti - Caso narrow",
-                                "Effetti misti con età categorica - Caso narrow",
-                                "Effetti fissi con età categorica - Caso narrow",
-                                "Effetti fissi con età categorica e SE robusti - Caso narrow")
+# names(model_results_indiv) <- c("Effetti misti con età in continuo - Caso narrow",
+#                                 "Effetti fissi con età in continuo - Caso narrow",
+#                                 "Effetti fissi con età in continuo e SE robusti - Caso narrow",
+#                                 "Effetti misti con età categorica - Caso narrow",
+#                                 "Effetti fissi con età categorica - Caso narrow",
+#                                 "Effetti fissi con età categorica e SE robusti - Caso narrow")
 
 save(model_results_indiv, file = file.path(thisdiroutput,"model_results_indiv.rda"))
 
+# Sensitivity analysis (considering only the first emorragic event per individual) ----
+
+input_sens <- input %>% 
+                filter(number_previous_bleedings==0)
+
+# estraggo denominatore (pazienti con emorragia)
+den_narrow_sens <- input_sens %>%
+                       filter(type_bleeding=="narrow") %>% 
+                       group_by(time) %>% 
+                       summarise(n_emor_narrow = n())
+
+den_poss_sens <- input_sens %>%
+                     filter(type_bleeding=="possible") %>% 
+                     group_by(time) %>% 
+                     summarise(n_emor_poss = n())
+
+# creo un dataset dove l'unità è il tempo (mese)
+
+# outcome_vars <- grep("^outcome", names(input), value = TRUE)
+outcome_vars <- c("outcome_THROM", "outcome_DEATH")
+
+results_sens <- map(outcome_vars, function(var) {
+  
+  input_sens %>%
+    group_by(time) %>% 
+    summarise(event_narrow = sum(.data[[var]]*(type_bleeding == "narrow"), na.rm = T),
+              event_poss = sum(.data[[var]]*(type_bleeding == "possible"), na.rm = T)) %>% 
+    mutate(period = case_when(time <= 26 ~ "1a",
+                              (time > 26 & time <= 41) ~ "1b",
+                              (time > 41 & time <= 44) ~ "1c",
+                              (time > 44 & time <= 67) ~ "2",
+                              time > 67 ~ "3"),
+           year = cut(time, breaks = seq(1,96, by = 12), labels = c(2018:2024), right = FALSE)) %>%
+    relocate(year, .before = time)
+  
+})
+
+names(results_sens) <- outcome_vars
+
+
+results_sens_updated <- list()
+
+for (i in names(results_sens)) {
+  
+  df <- results_sens[[i]] 
+  
+  results_sens_updated[[i]] <- df %>% 
+    bind_rows(fix_time %>% select(-month) %>% anti_join(results_sens[[i]], by = c("year", "time", "period"))) %>% 
+    group_by(year) %>% 
+    mutate(month = 1:12) %>% 
+    relocate(month, .after = year) %>%
+    # ungroup() %>%
+    left_join(den_poss_sens, by = "time") %>%
+    left_join(den_narrow_sens, by = "time") %>%
+    mutate(prop_poss = event_poss/n_emor_poss,
+           prop_narrow = event_narrow/n_emor_narrow)
+  
+}
+
+combined_data <- bind_rows(lapply(names(results_sens_updated), function(name) {
+  results_sens_updated[[name]] %>% 
+    mutate(outcome = name)
+}), .id = "id")
+
+# Plots ----
+
+png(file.path(thisdiroutput,"outcomes_possible_sens.png"), units="in", height=10, width=15, res=300)
+
+# scatter plot separati per outcome (nel tempo)
+ggplot(combined_data, aes(x = time, y = prop_poss)) +
+  geom_line(aes(color = outcome), size = 1) +
+  geom_point(size = 1) +
+  labs(
+    title = "Andamento della proporzione degli outcome di interesse (def. possible) - Solo primo outcome emorragico",
+    x = "Tempo, mesi",
+    y = "Proporzione"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  facet_wrap(~outcome, scales = "free_y")
+
+dev.off()
+
+png(file.path(thisdiroutput,"outcomes_narrow_sens.png"), units="in", height=10, width=15, res=300)
+
+# scatter plot separati per outcome (nel tempo)
+ggplot(combined_data, aes(x = time, y = prop_narrow)) +
+  geom_line(aes(color = outcome), size = 1) +
+  geom_point(size = 1) +
+  labs(
+    title = "Andamento della proporzione degli outcome di interesse (def.narrow) - Solo primo outcome emorragico",
+    x = "Tempo, mesi",
+    y = "Proporzione"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
+  facet_wrap(~outcome, scales = "free_y")
+
+dev.off()
+
+# analysis with time as unit ----
+
+model_results_sens <- map(results_sens_updated, fit_models)
+save(model_results_sens, file = file.path(thisdiroutput,"model_results_sens.rda"))
+
+# analysis with individual as unit ----
+
+model_results_indiv_sens <- list(
+  
+  fit_indiv_narrow_mixed <- summary(glmer(outcome_DEATH ~ period + age + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input_sens)),
+  fit_indiv_narrow_fixed <- summary(glm(outcome_DEATH ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input_sens)),
+  fit_indiv_narrow_mixed_agecat <- summary(glmer(outcome_DEATH ~ period + ageband + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input_sens)),
+  fit_indiv_narrow_fixed_agecat <- summary(glm(outcome_DEATH ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input_sens)),
+  
+  fit_indiv_narrow_mixed <- summary(glmer(outcome_THROM ~ period + age + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input_sens)),
+  fit_indiv_narrow_fixed <- summary(glm(outcome_THROM ~ period + age + gender, subset = type_bleeding == "narrow", family = binomial, data = input_sens)),
+  fit_indiv_narrow_mixed_agecat <- summary(glmer(outcome_THROM ~ period + ageband + gender + (1|person_id), subset = type_bleeding == "narrow", family = binomial, data = input_sens)),
+  fit_indiv_narrow_fixed_agecat <- summary(glm(outcome_THROM ~ period + ageband + gender, subset = type_bleeding == "narrow", family = binomial, data = input_sens)),
+  
+)
+
+# names(model_results_indiv) <- c("Effetti misti con età in continuo - Caso narrow",
+#                                 "Effetti fissi con età in continuo - Caso narrow",
+#                                 "Effetti fissi con età in continuo e SE robusti - Caso narrow",
+#                                 "Effetti misti con età categorica - Caso narrow",
+#                                 "Effetti fissi con età categorica - Caso narrow",
+#                                 "Effetti fissi con età categorica e SE robusti - Caso narrow")
+
+save(model_results_indiv_sens, file = file.path(thisdiroutput,"model_results_indiv_sens.rda"))
+
+# Descriptive table with number of death, thromboembolic and emorragic events by period ----
+
+num_death <- input %>% 
+  group_by(period) %>% 
+  summarise(num_death = sum(outcome_DEATH))
+
+num_throm <- input %>% 
+  group_by(period) %>% 
+  summarise(num_throm = sum(outcome_THROM))
+  
+num_emorr <- input %>% 
+  group_by(period) %>% 
+  summarise(num_emorr = n())
+
+num_emorr_solo_prima <- input_sens %>% 
+  group_by(period) %>% 
+  summarise(num_emorr_prima = n())
+
+tables <- list(num_death, num_throm, num_emorr, num_emorr_solo_prima)
+
+descr_tab <- purrr::reduce(tables, inner_join, by = "period")
+
+save(descr_tab, file = file.path(thisdiroutput,"descr_tab.rda"))
 
 # processing <- ...
   
