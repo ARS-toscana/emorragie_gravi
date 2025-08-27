e9e104827250cc8dbf727447731ab84d24ed15be
sabrinagiometto <sabrina.giometto@gmail.com>
2025-08-27 15:01:15 +0200

    tolerance window of 1 day reduced to only one direction
    double round matching implemented

--- p_steps/06_T2_30_probabilistic_linkage.R
+++ p_steps/06_T2_30_probabilistic_linkage.R
@@ -1,9 +1,20 @@
-# if (!requireNamespace("officer", quietly = TRUE)) install.packages("officer")
-# if (!requireNamespace("flextable", quietly = TRUE)) install.packages("flextable")
-# library(officer)
-# library(flextable)
+# if (!requireNamespace("gt", quietly = TRUE)) install.packages("gt")
+# if (!requireNamespace("gtsummary", quietly = TRUE)) install.packages("gtsummary")
+# library(gt)
+# library(gtsummary)
 
 # Probabilistic linkage ----
+
+# authors: Sabrina Giometto
+
+# v 0.1 13 Aug 2025 - First attempt of probabilistic linkage after removing rows in fes with negative Nvialsday & Ndispday
+
+# v 0.2 27 Aug 2025 - Refining record linkage: 
+# - reduce tolerance window of 1 day to only one direction (fes date after bleeding date);
+# - remove rows with exact match (1 to 1) and then move to second round matching (with tolerance);
+# - print new descriptive tables with more details and stratified by type of bleeding;
+
+#############################
 
 if (TEST){
   testname <- "test_D3_dispensings_AA"
@@ -30,64 +41,78 @@ D3_study_population <- readRDS(file.path(thisdirinput, "D3_study_population.rds"
 # remove negative records
 D3_dispensings_AA <- D3_dispensings_AA[Nvialsday>0 & Ndispday>0]
 
-# initialize list including m datasets, one for each tolerance value
-matched_data <- list()
+# reshape dataset with bleedings with unique rows per date of bleeding
+counts <- D3_study_population[,.(N = .N), by = date_bleeding]
 
-# set tolerate gap in days
-tolerance <- c(0:3)
+#?? in caso di date di bleeding coincidenti, quale tengo (e quindi coi caratterizzo) e quale butto? Tutte. Tengo solo quelle non ripetute
+D3_study_population_u <- D3_study_population[date_bleeding %in% counts[N == 1, date_bleeding]]
 
-for (i in seq_along(tolerance)) {
-  
-  # create time interval for dispensing dates
-  D3_dispensings_AA_i <- copy(D3_dispensings_AA)
-  D3_dispensings_AA_i <- D3_dispensings_AA_i[, `:=`(data_min = data - tolerance[i],
-                                                    data_max = data + tolerance[i])]
-  
-  D3_dispensings_AA_i <- D3_dispensings_AA_i[, id_dispensing:= 1:.N]
-  
-  # create artificial start and end dates equal to the bleeding date
-  # data_2 <- copy(D3_study_population)
-  data_2 <- copy(D3_study_population)
-  data_2[, `:=`(date_start = date_bleeding, date_end = date_bleeding)]
-  
-  setkey(data_2, date_start, date_end)
-  setkey(D3_dispensings_AA_i, data_min, data_max)
-  
-  # full join non-equi
-  left_bleed  <- foverlaps(data_2, D3_dispensings_AA_i,
-                      by.x = c("date_start", "date_end"),
-                      by.y = c("data_min", "data_max"),
-                      type = "within", nomatch = NA)
-  
-  left_disp <- foverlaps(D3_dispensings_AA_i, data_2,
-                              by.x = c("data_min", "data_max"),
-                              by.y = c("date_start", "date_end"),
-                              type = "any", nomatch = NA)
-  
-  bleed_only <- left_bleed %>% filter(is.na(data))
-  disp_only <- left_disp %>% filter(is.na(date_bleeding))
-  matched <- left_bleed %>% filter(!is.na(data))
-  # matched_check <- left_disp %>% filter(!is.na(date_bleeding))
-  
-  # 3) unisci i due dataset con rbind, eliminando duplicati
-  data_m <- rbindlist(list(bleed_only, disp_only, matched), fill=TRUE)
-  
-  data_m[, c("date_start", "date_end", "data_min", "data_max") := NULL]
-  
-  data_m <- data_m %>% 
-    relocate(person_id, .before = "data") %>% 
-    relocate(date_bleeding, .after = "person_id") %>% 
-    rename(date_disp_AA = data)
-  
-  matched_data[[i]] <- data_m
-  
-  names(matched_data)[i] <- paste("tolerance", tolerance[i], sep = "_")
-  
-  
-}
+#?? quindi butto anche righe di fes con date coincidenti? NO(?!)
+# D3_dispensings_AA_u <- D3_dispensings_AA[!duplicated(data) ,]
 
+# create id dispensing
+D3_dispensings_AA <- D3_dispensings_AA[, id_dispensing:= 1:.N]
 
-### descriptive table ----
+# exact matching
+match_exact <- D3_dispensings_AA[D3_study_population_u, on = .(data = date_bleeding), nomatch = 0L]
+
+# create 1° and 2° round matching by excluding those matched 1 to 1 with the exact matching
+ids <- match_exact[, person_id]
+
+# extract ids to be matched at 2° round
+D3_study_population_u2 <- D3_study_population_u[!person_id %in% ids ,]
+
+# perform 2° round matching with 1 day of tolerance (only in one direction)
+tolerance <- 1
+
+D3_dispensings_AA <- D3_dispensings_AA[, `:=`(data_min = data - tolerance,
+                                                  data_max = data)]
+
+# create artificial start and end dates equal to the bleeding date
+D3_study_population_u2[, `:=`(date_start = date_bleeding, date_end = date_bleeding)]
+
+setkey(D3_study_population_u2, date_start, date_end)
+setkey(D3_dispensings_AA, data_min, data_max)
+
+# full join non-equi
+left_bleed  <- foverlaps(D3_study_population_u2, D3_dispensings_AA,
+                         by.x = c("date_start", "date_end"),
+                         by.y = c("data_min", "data_max"),
+                         type = "within", nomatch = NA)
+
+left_disp <- foverlaps(D3_dispensings_AA, D3_study_population_u2,
+                       by.x = c("data_min", "data_max"),
+                       by.y = c("date_start", "date_end"),
+                       type = "any", nomatch = NA)
+
+bleed_only <- left_bleed %>% filter(is.na(data))
+disp_only <- left_disp %>% filter(is.na(date_bleeding))
+matched <- left_bleed %>% filter(!is.na(data))
+# matched_check <- left_disp %>% filter(!is.na(date_bleeding))
+
+# unisci i due dataset con rbind, eliminando duplicati
+match_2_round <- rbindlist(list(bleed_only, disp_only, matched), fill=TRUE)
+
+match_2_round[, c("date_start", "date_end", "data_min", "data_max") := NULL]
+
+match_2_round <- match_2_round %>% 
+  relocate(person_id, .before = "data") %>% 
+  relocate(date_bleeding, .after = "person_id") 
+
+# create tag variables to identify matching quality
+match_exact <- match_exact[, `:=`(is_1_match = 1,
+                                  is_2_match = 0)]
+
+match_2_round <- match_2_round[, `:=`(is_1_match = 0,
+                                      is_2_match = fifelse(!is.na(data) & !is.na(date_bleeding), 1, 0))]
+
+# bind ids rows with 1°round matching and those with 2° round matching
+match_df <- rbind(match_exact[, .(person_id, type_bleeding, is_1_match, is_2_match, data, id_dispensing, Nvialsday)], match_2_round[, .(person_id, type_bleeding, is_1_match, is_2_match, data, id_dispensing, Nvialsday)])
+
+# create exposure variable
+match_df <- match_df[, is_exposed := fifelse(is_1_match == 1 | is_2_match == 1, 1, 0)]
+
+# 0) descriptive table general
 
 D3_dispensings_AA[, tag := NA]
 D3_dispensings_AA[!is.na(data), tag := duplicated(data)]
@@ -95,71 +120,52 @@ D3_dispensings_AA[!is.na(data), tag := duplicated(data)]
 D3_study_population[, tag := NA]
 D3_study_population[!is.na(date_bleeding), tag := duplicated(person_id, date_bleeding)]
 
+
 descriptive_table <- list(
   
-  "Number of rows of D3_study_population dataset (bleeding as unit)" = D3_study_population[,uniqueN(data.table(person_id, date_bleeding))],
+  "Number of rows of D3_study_population dataset (bleeding as unit)" = nrow(D3_study_population),
   "Number of rows of dispensing_AA dataset (dispensing day and hospital as unit)" = nrow(D3_dispensings_AA),
   "Number of bleeding dates replicated in D3_study_population" = D3_study_population[, .N, by = tag],
-  "Number of dispensing dates replicated in D3_dispensings_AA" = D3_dispensings_AA[, .N, by = tag]
+  "Number of unique dispensing dates" = D3_dispensings_AA[, uniqueN(data)],
+  "Number of dispensing dates replicated in D3_dispensings_AA" = D3_dispensings_AA[, .N, by = tag],
+  "Number of rows of the matched dataset (inner join at 1 round and full join at 2 round)" = nrow(match_df)
   
 )
-  
-for (i in seq_along(tolerance)) {
-  
-  bleed_summary <- matched_data[[i]][!is.na(person_id) & !is.na(date_bleeding), .(n_fes = if (all(is.na(date_disp_AA))) 0L else .N), by = .(person_id, date_bleeding)]
 
-  setorder(bleed_summary, n_fes)   
-  descriptive_table[[paste("Number of rows of matched dataset", "tolerance", tolerance[i], sep = "_")]] = nrow(matched_data[[i]])
-  # descriptive_table[[paste("Number of IDs without a matched dispensing", "tolerance", tolerance[i], sep = "_")]] = matched_data[[i]][is.na(date_disp_AA)][!duplicated(person_id), .N]
-  # descriptive_table[[paste("Number of IDs with a matched dispensing", "tolerance", tolerance[i], sep = "_")]] = matched_data[[i]][!is.na(date_disp_AA)][!duplicated(person_id), .N] 
-  
-  descriptive_table[[paste("Bleedings categorized per number of matches", "tolerance", tolerance[i], sep = "_")]] = bleed_summary[, .N, by = .(n_fes)]
-  
-  # non distinguo però il caso in cui ho una data di dispensazione ripetuta solo una volta o più
-  dispensing_summary <- matched_data[[i]][!is.na(date_disp_AA), .(n_bleedings = if (all(is.na(date_bleeding))) 0L else uniqueN(data.table(person_id, date_bleeding))), by = .(date_disp_AA)]
-  dispensing_summary <- matched_data[[i]][!is.na(date_disp_AA), .(n_bleedings = if (all(is.na(date_bleeding))) 0L else .N), by = .(id_dispensing)]
-  
-  setorder(dispensing_summary, n_bleedings)
-  
-  descriptive_table[[paste("Dispensing in fes categorized per number of matches", "tolerance", tolerance[i], sep = "_")]] = dispensing_summary[, .N, by = .(n_bleedings)]
-  
-  # matched_data[[i]][, tag := NA]
-  # matched_data[[i]][!is.na(date_disp_AA), tag := duplicated(date_disp_AA)]
-  # 
-  # descriptive_table[[paste("Number of dispensing dates replicated in the matched dataset", "tolerance", tolerance[i], sep = "_")]] = matched_data[[i]][, .N, by = tag]
-  
 
-}
+# 1) descriptive table bleedings
 
-  
-descriptive_table
+today <- format(Sys.Date(), "%Y%m%d")
 
-# print table
-# doc <- read_docx()
-# 
-# for (i in names(descriptive_table)) {
-#      
-#       tmp <- descriptive_table[[i]]
-#       
-#       if (is.data.frame(tmp)) {
-#         
-#         df <- tmp
-#         if (ncol(df) == 1) names(df) <- "N"
-#         
-#       } else {
-#         
-#         df <- data.frame(N = tmp, check.names = FALSE)
-#         
-#       }
-#           
-#       doc <- doc %>%
-#             body_add_par(i, style = "heading 1") %>%
-#             body_add_flextable(flextable(df)) %>%
-#             body_add_par("", style = "Normal")
-#             
-# }
-# 
-# print(doc, target = file.path(thisdiroutput,"descriptive_post_prob_linkage.docx"))
+tab <- match_df[!is.na(type_bleeding) & !duplicated(person_id)]
+
+tbl1 <- tab |> tbl_summary(by = type_bleeding, include = is_exposed) |>
+       add_overall() |> 
+       as_gt() |> 
+       gt::gtsave(filename = file.path(thisdiroutput,paste0("tbl_all", today, ".docx")))
+
+tab_exposed <- tab[is_exposed==1 ,]
+
+tbl2 <- tab_exposed |> tbl_summary(by = type_bleeding, include = c(is_1_match, is_2_match)) |>
+               add_overall() |> 
+               as_gt() |> 
+               gt::gtsave(filename = file.path(thisdiroutput,paste0("tbl_exposed", today, ".docx")))
+
+# 2) descriptive table fes
+
+dispensing_summary <- match_df[!is.na(data), .(n_bleedings = if (all(is.na(type_bleeding))) 0L else .N), by = id_dispensing]
+
+setorder(dispensing_summary, n_bleedings)
+
+descriptive_table[["Dispensing in fes categorized per number of matches"]] = dispensing_summary[, .N, by = .(n_bleedings)]
+
+setorder(D3_dispensings_AA, Nvialsday)
+
+descriptive_table[["Distribution of Nvials per dispensing in fes"]] = D3_dispensings_AA[, .N, by = .(Nvialsday)]
+
+# descriptive_table
+
 saveRDS(descriptive_table, file = file.path(thisdiroutput,"descriptive_post_prob_linkage.rds"))
+
 
 ## 2) matching keeping negative values for dispensings/vials ----
